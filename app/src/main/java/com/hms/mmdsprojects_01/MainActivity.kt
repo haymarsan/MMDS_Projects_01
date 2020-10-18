2e0e3cc849ae9a3c358a4a539d8a9d4e6bb0f1d2
Hay Mar San <haymarsan11@gmail.com>
2020-10-19 01:45:56 +0630

    features: fill your information UI update,

--- app/src/main/java/com/hms/mmdsprojects_01/MainActivity.kt
+++ app/src/main/java/com/hms/mmdsprojects_01/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_set_password)
+        setContentView(R.layout.activity_fill_information)
     }
 }
