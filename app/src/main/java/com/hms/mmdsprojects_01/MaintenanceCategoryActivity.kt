6458b45d0ed813c4c080e3c55ce087c6051c0370
Hay Mar San <haymarsan11@gmail.com>
2021-02-13 00:30:44 +0630

    features: activity_maintenance_category.xml UI added,

---
+++ app/src/main/java/com/hms/mmdsprojects_01/MaintenanceCategoryActivity.kt
@@ -0,0 +1,11 @@
+package com.hms.mmdsprojects_01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MaintenanceCategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maintenance_category)
+    }
+}
